36c23bcb85204080d208e16f99afe32b53ba4f8a
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-07-27 01:23:49 -0400

    Stuff

--- bentley-ottmann/script.js
+++ bentley-ottmann/script.js
@@ -1,3 +1,27 @@
+// Polyfill for the Array.findIndex()
+if (!Array.prototype.findIndex) {
+  Array.prototype.findIndex = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.findIndex called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
 /**
  * Create a line segment.
  *
@@ -22,16 +46,34 @@ lineSegment = function(point1, point2) {
 			this.rightEndpoint = point1;
 		}
 	}
+	this.slope = ( this.rightEndpoint.y - this.leftEndpoint.y ) / (this.rightEndpoint.x - this.leftEndpoint.x);
+	this.b = this.leftEndpoint.y - (slope) * this.leftEndpoint.x;
+	/**
+	 * Get the y coordinate of a spot on the line given an x coordinate.
+	 *
+	 * Use slope-intercept formula for fast processing.
+	 *
+	 * @param  {[type]} x [description]
+	 * @return {[type]}   [description]
+	 */
+	this.y = function(x) {
+		return slope * x + b;
+	};
 	return this;
 };
 
+function whereTwoPointsIntersect = function(point1, point2) {
+	// ?
+}
+
 var lineSegments = [];
-for ( i = 0; i <=100; i++ ) {
+for ( i = 0; i <=5; i++ ) {
 	lineSegments.push( new lineSegment(
-		{ x: Math.ceil( 100* Math.random() ), y: Math.ceil( 100* Math.random() ) },
-		{ x: Math.ceil( 100* Math.random() ), y: Math.ceil( 100* Math.random() ) }
+		{ x: Math.ceil( 500 * Math.random() ), y: Math.ceil( 500 * Math.random() ) },
+		{ x: Math.ceil( 500 * Math.random() ), y: Math.ceil( 500 * Math.random() ) }
 	) );
 }
+
 /**
  * Create and return event queue.
  *
@@ -63,24 +105,120 @@ function EventQueue() {
 
 eq = EventQueue();
 
+//     Initialize event queue EQ = all segment endpoints;
 lineSegments.forEach(function(lineSegment) {
 	eq.push({
+		type: 'leftEndpoint',
 		x: lineSegment.leftEndpoint.x,
-		y: lineSegment.leftEndpoint.y
+		y: lineSegment.leftEndpoint.y,
+		lineSegment: lineSegment
 	});
 	eq.push({
+		type: 'rightEndpoint',
 		x: lineSegment.rightEndpoint.x,
-		y: lineSegment.rightEndpoint.y
+		y: lineSegment.rightEndpoint.y,
+		lineSegment: lineSegment
 	});
 });
+//     Sort EQ by increasing x and y;
 eq.sort();
 
-var sweepLine = {};
+var SweepLine = function() {
+	this.lineSegments = [];
+	this.x = 0;
+	this.sort = function() {
+		var self = this;
+		return this.lineSegments.sort(function(segment1, segment2) {
+			if ( segment1.y(self.x) > segment2.y(self.x) ) {
+				return -1;
+			} else {
+				return 1;
+			}
+		});
+	};
+	this.findIndexFromValue = function(value) {
+		return this.lineSegments.findIndex(function(item) {
+			return value == item;
+		});
+	};
+	this.push = function(item) {
+		this.lineSegments.push(item);
+		this.sort();
+		var found = this.findIndexFromValue(item);
+		return found;
+	};
+	this.before = function(index) {
+		if ( this.lineSegments[index-1] ) {
+			return this.lineSegments[index-1];
+		} else {
+			return false;
+		}
+	};
+	this.after = function(index) {
+		if ( this.lineSegments[index+1] ) {
+			return this.lineSegments[index+1];
+		} else {
+			return false;
+		}
+	};
+	this.remove = function(index) {
+		this.lineSegments.splice(index-1, 1 );
+	};
+	return this;
+};
+//     Initialize sweep line SL to be empty;
+var sweepLine = new SweepLine();
 while ( eq.length ) {
 	var event = eq[0];
-
+	sweepLine.x = event.x;
+	sweepLine.sort();
+	var lineSegment = event.lineSegment;
+	if ( event.type == 'leftEndpoint' ) {
+		var index = sweepLine.push(lineSegment);
+		var before = sweepLine.before(index);
+		var after = sweepLine.after(index);
+		if ( before && lineSegmentsIntersect( {x:lineSegment.leftEndpoint.x, y:lineSegment.leftEndpoint.y},
+		                                      {x:lineSegment.rightEndpoint.x, y:lineSegment.rightEndpoint.y},
+		                                      {x:before.leftEndpoint.x, y:before.leftEndpoint.y},
+		                                      {x:before.rightEndpoint.x, y:before.rightEndpoint.y} ) ) {
+			eq.push()
+		}
+		if ( after && lineSegmentsIntersect( {x:lineSegment.leftEndpoint.x, y:lineSegment.leftEndpoint.y},
+		                                      {x:lineSegment.rightEndpoint.x, y:lineSegment.rightEndpoint.y},
+		                                      {x:after.leftEndpoint.x, y:after.leftEndpoint.y},
+		                                      {x:after.rightEndpoint.x, y:after.rightEndpoint.y} ) ) {
+			console.log('intersection!');
+		}
+	} else if ( event.type == 'rightEndpoint' ) {
+		var index = sweepLine.findIndexFromValue(lineSegment);
+		var before = sweepLine.before(index);
+		var after = sweepLine.after(index);
+		sweepLine.remove(index);
+		if ( before && after && lineSegmentsIntersect( {x:after.leftEndpoint.x, y:after.leftEndpoint.y},
+		                                      {x:after.rightEndpoint.x, y:after.rightEndpoint.y},
+		                                      {x:before.leftEndpoint.x, y:before.leftEndpoint.y},
+		                                      {x:before.rightEndpoint.x, y:before.rightEndpoint.y} ) ) {
+			console.log('intersection!');
+		}
+	}
 	eq.shift();
 }
+var svg = document.querySelector('svg');
+lineSegments.forEach(function(line) {
+	var svgline = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+	svgline.setAttribute('x1', line.leftEndpoint.x);
+	svgline.setAttribute('y1', line.leftEndpoint.y);
+	svgline.setAttribute('x2', line.rightEndpoint.x);
+	svgline.setAttribute('y2', line.rightEndpoint.y);
+	// if ( line.hasIntersect ) {
+		var color = 'red', width = 2;
+	// } else {
+		// var color = 'blue', width = 10;
+	// }
+	svgline.setAttribute('stroke', color);
+	svgline.setAttribute('stroke-width', width);
+	svg.appendChild(svgline);
+});
 // var EventQueue = function(lineSegments) {
 // 	this.events = [];
 // 	lineSegments.forEach(function(lineSegment) {
@@ -96,10 +234,6 @@ while ( eq.length ) {
 // };
 
 // var eq = new EventQueue(lineSegments);
-
-//     Initialize event queue EQ = all segment endpoints;
-//     Sort EQ by increasing x and y;
-//     Initialize sweep line SL to be empty;
 //     Initialize output intersection list IL to be empty;
 
 //     While (EQ is nonempty) {
